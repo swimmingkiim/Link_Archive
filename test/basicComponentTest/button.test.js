928e30ab88d9f853070accf063f310668be23cd3
swimmingkiim <swimmingkiim@gmail.com>
2020-06-25 17:55:51 +0900

    completed button component test on checking properties and onClick functions

---
+++ test/basicComponentTest/button.test.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { create } from "react-test-renderer";
+import Button from "../../app/src/basicComponent/Button";
+
+const createJSXForButton = (type, updateId = null) => {
+  return <Button type={type} updateId={type === "update" ? 12345 : null} />;
+};
+
+describe("test Button's properties and onClick functions by passing the type", () => {
+  const typesOfButton = ["save", "goBack", "edit", "update", "delete"];
+
+  test.each(typesOfButton)(
+    "test properties of basic %s button",
+    (buttonType, done) => {
+      const testButton = create(createJSXForButton(buttonType)).toTree();
+      expect(testButton.rendered.props.className).toStrictEqual(
+        expect.any(String)
+      );
+      expect(testButton.rendered.props.type).toBe(
+        buttonType === "save" ? "submit" : "button"
+      );
+      expect(testButton.rendered.props.onClick).not.toBeNull();
+      expect(testButton.rendered.props.children).toBe(
+        buttonType === "goBack" ? "go back" : buttonType
+      );
+      if (buttonType == "update") {
+        expect(testButton.rendered.props["data-post-id"]).toBe("12345");
+      } else {
+        expect(testButton.rendered.props["data-post-id"]).toBe("null");
+      }
+      done();
+    }
+  );
+
+  test.each(typesOfButton)(
+    "test if %s's onClick function are working properly",
+    (buttonType, done) => {
+      const homeAddress = "http://localhost";
+      const currentAddress = window.location.href;
+      const testButton = create(createJSXForButton(buttonType));
+      const mockOnClick = jest.fn(testButton.onClick);
+      mockOnClick();
+      expect(mockOnClick.mock.calls.length).toBe(1);
+      switch (buttonType) {
+        case "save":
+          expect(window.location.href).toBe(homeAddress + "/");
+        case "goBack":
+          expect(window.location.href).toBe(currentAddress);
+        case "edit":
+        // expect(document.getElementById("main-list").classList).toContain(
+        //   "edit-mode"
+        // );
+        case "update":
+        // at the moment, window.location.href is throw 404, so activate after writing <Form />
+        // expect(window.location.href).toBe(homeAddress + "/post/12345");
+        case "delete":
+          expect(window.location.href).toBe(homeAddress + "/");
+      }
+      done();
+    }
+  );
+});
